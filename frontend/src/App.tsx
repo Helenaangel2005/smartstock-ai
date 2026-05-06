8c2e34b1ab1731e7452e5264c5632df40f04a4b9
Helenaangel2005 <helenafaustino375@gmail.com>
2026-05-06 19:26:02 +0530

    Added KPI cards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Hero from "./sections/Hero";
 import Alerts from "./components/Alerts";
+import KPICards from "./components/KPICards";
 
 function App() {
 
@@ -19,13 +20,21 @@ function App() {
       />
 
       {/* MAIN */}
-      <div className="flex-1 p-6 space-y-6">
+      <div className="flex-1 p-6 space-y-6 overflow-y-auto">
 
         {/* HERO */}
         <Hero product={product} />
 
         {/* ALERTS */}
         <Alerts />
+
+        {/* PRODUCT TITLE */}
+        <h2 className="text-3xl font-bold text-gray-800">
+          Product: {product}
+        </h2>
+
+        {/* KPI CARDS */}
+        <KPICards product={product} />
 
       </div>
 

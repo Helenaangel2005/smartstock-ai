8988b4445332138b9005bdd6b3cba7ca03eeeebf
Helenaangel2005 <helenafaustino375@gmail.com>
2026-05-06 19:13:05 +0530

    Testing sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import { useState } from "react";
+
+import Sidebar from "./components/Sidebar";
 
 function App() {
+
+  const [product, setProduct] = useState("Milk");
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-5xl font-bold text-green-700">
-        SmartStock AI Working 🚀
-      </h1>
+
+    <div className="flex bg-gray-100 min-h-screen">
+
+      <Sidebar
+        selected={product}
+        setProduct={setProduct}
+      />
+
+      <div className="flex-1 p-6">
+
+        <h1 className="text-5xl font-bold text-green-700">
+          SmartStock AI Working 🚀
+        </h1>
+
+      </div>
+
     </div>
+
   );
 }
 
